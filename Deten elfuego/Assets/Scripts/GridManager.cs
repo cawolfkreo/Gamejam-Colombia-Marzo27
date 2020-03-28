00b978d7672d333dc24a954ea5d5c5f6d3ec48db
Camilo Zambrano <camilozambranovotto@hotmail.com>
2020-03-28 11:57:39 -0500

    Grid Base script
    this is just a small step for the grid base system. I will continue with it in a few minutes :suspect:

---
+++ Deten elfuego/Assets/Scripts/GridManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    //The number of columns the grid is going to have.
+    [SerializeField]
+    private int numXCells = 10;
+
+    //The number of rows the grid is going to have.
+    [SerializeField]
+    private int numYCells = 10;
+
+    //The size a single cell is going to have.
+    [SerializeField]
+    private float cellSize = 5f;
+    
+    //The grid
+    private Grid grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new Grid(numXCells, numXCells, cellSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(grid.test());
+    }
+}
