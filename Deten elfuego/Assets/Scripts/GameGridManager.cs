ce4c22547ae529b2ea5cede38f907af059a5e75e
Yesid  Bejarano <yabcyesid12@gmail.com>
2020-03-28 20:31:48 -0500

    inicio posiciones escnea jugador

---
+++ Deten elfuego/Assets/Scripts/GameGridManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGridManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GridManager gridManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gridManager = new GridManager();
+        var mapObjects = GameObject.FindGameObjectsWithTag("MapObject");
+
+        foreach (GameObject mapObject in mapObjects)
+        {
+            foreach (Transform child in mapObject.transform)
+            {
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
