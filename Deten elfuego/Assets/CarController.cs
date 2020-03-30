e5a6f6fbbd8ecc22c0f7b4e1ed9971936c91ab07
jmmunoz14 <jm.munoz14@uniandes.edu.co>
2020-03-29 20:18:25 -0500

    finished car animation

---
+++ Deten elfuego/Assets/CarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animation;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "door")
+        {
+            animation = GetComponent<Animator>();
+            animation.Play("car chain");
+            Debug.Log("entra al collision car");
+        }
+
+    }
+}
---
+++ Deten elfuego/Assets/JumpController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animation;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "cube")
+        {
+            animation = GetComponent<Animator>();
+            animation.Play("CarJump");
+            Debug.Log("entra al collision car");
+        }
+
+    }
+}
