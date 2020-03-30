5b246b86f6f22433b582daf42462b29d56b9503b
jmmunoz14 <jm.munoz14@uniandes.edu.co>
2020-03-30 14:06:15 -0500

    book animation update.

---
+++ Deten elfuego/Assets/WinCondition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject canvas;
+
+    private bool isFrozen;
+
+    private Vector3 vectorPosition;
+
+
+    void Update()
+    {
+        if(isFrozen )
+        {
+            transform.position = vectorPosition;
+        }
+    }
+
+    void Start()
+    {
+        isFrozen = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "car")
+        {
+            vectorPosition = transform.position;
+            isFrozen = true;
+            canvas.SetActive(true);
+            Debug.Log("ganste");
+            Debug.Log(gameObject.name);
+        }
+
+    }
+
+}
